22ca11a9cfb6a17e14d3b8e529c36a45067e30e1
Mauri_JSX <prv_mauricio.torres@dale.com.co>
2024-03-07 13:03:00 -0300

    Added good solution and cute comments

---
+++ FrecuencyCounter/03-MultiplePointerPattern.ts
@@ -0,0 +1,32 @@
+//? Return the first pair of number who sum 0 together.
+
+type SumType = (array: number[]) => number[] | undefined;
+
+//! Bad solution
+const sumNumberNaiveSolution: SumType = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === 0) {
+        return [array[i], array[j]];
+      }
+    }
+  }
+};
+
+//* Best solution 
+const sumZero = (array) => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    let sum = array[left] + array[right];
+
+    if (sum === 0) {
+      return [array[left], array[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left--;
+    }
+  }
+};
