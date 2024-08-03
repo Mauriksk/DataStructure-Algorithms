8f06c664acffed8cc356ccd299024df04f673c30
Mauriksk <otarksk@gmail.com>
2024-08-03 06:44:38 -0300

    NaiveSearch2

--- Search/StringSearch.ts
+++ Search/StringSearch.ts
@@ -17,3 +17,18 @@ const naiveSearch = (chain: string, slice: string): number => {
 }
 
 console.log(naiveSearch("omglittleboyomg", "omg"))// 2 times
+
+//Second Naive Search
+
+function NaiveSearch2 (long: string, short: string) {
+    let count = 0;
+
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if(short[j] !== long[i + j]) break
+            if(j === short.length - 1) count++
+            
+        }
+    }
+    return count
+}
