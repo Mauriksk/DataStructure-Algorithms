63be238b6113ade17deae538bc4ab55c02c4e8c9
Mauriksk <otarksk@gmail.com>
2024-08-08 22:58:44 -0300

    change file

---
+++ Sort/BubbleSort/swapFunctions.ts
@@ -0,0 +1,10 @@
+function swap(arr: [], x: number, y: number) {
+    const temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+  }
+  
+  const swapES6 = (arr: [], x: number, y: number) => {
+    [arr[x], arr[y]] = [arr[y], arr[x]];
+  };
+  
